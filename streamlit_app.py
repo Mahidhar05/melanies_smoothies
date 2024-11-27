942db95d01f20975f8797227a4c8457ce8430fdf
V.S.D.S.Mahidhar <61974498+Mahidhar05@users.noreply.github.com>
2024-11-27 13:46:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 # Import python packages
+python3 -m pip install pip --upgrade
+pip install pyopenssl --upgrade
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
